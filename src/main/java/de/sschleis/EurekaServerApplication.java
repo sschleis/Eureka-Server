3397c080fb040506d6677b9f140d5125222f2ea9
sschleis <sebastian.schleis@gmail.com>
2016-08-24 00:26:52 +0200

    Init

---
+++ src/main/java/de/sschleis/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package de.sschleis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringCloudApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
